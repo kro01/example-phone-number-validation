d74b3844535f25f2aa95c8c33deacbdd346fd6c6
Konstantin <kedimitrov@gmail.com>
2015-07-28 02:55:09 +0300

    email validation

--- us_phone_number.py
+++ us_phone_number.py
@@ -4,3 +4,15 @@ for test_string in ['555-1212', 'ILL-EGAL']:
         print (test_string + ' is a valid US local phone number')
     else:
         print (test_string + ' is rejected')
+
+for test_string in [' user.user.user-user u@example.com',' user.user.user-@example.com',' user@example.com']:
+    if not  re.match(r'^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$',test_string):
+        print(test_string + ' is a valid email')   
+    else:
+        print (test_string + ' is rejected')
+
+for test_string in ['user.user.user-user u@examp.le.com','user.user.user-@example.com','user@example.com']:
+    if not  re.match(r'^[A-Za-z0-9\.]@[A-Za-z0-9]\.[a-zA-Z]*$',test_string):
+        print(test_string + ' is a valid email')   
+    else:
+        print (test_string + ' is rejected')
